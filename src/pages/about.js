2cb2cf7ef87f80a1d81da8eaab50d76e8d6a1856
Oleksandr Bilyi <o.d.bilyi@gmail.com>
2021-03-12 19:44:38 +0200

    added about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { FirstSection } from '../components/About/FirstSection';
 import { SecondSection } from '../components/About/SecondSection';
+import { ThirdSection } from '../components/About/ThirdSection';
+import { OurContacts } from '../components/OurContacts/OurContacts';
 
 const AboutPage = () => {
   return (
     <React.Fragment>
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
+      <OurContacts />
     </React.Fragment>
   )
 };
